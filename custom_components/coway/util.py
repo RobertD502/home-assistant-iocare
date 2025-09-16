dde95cdf5155fb239b5cfd085d67cd76bd853ad9
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2025-09-16 15:13:42 -0400

    add ServerMaintenance exception, improve readability

--- custom_components/coway/util.py
+++ custom_components/coway/util.py
@@ -5,7 +5,7 @@ import async_timeout
 
 from aiohttp import ClientSession
 from cowayaio import CowayClient
-from cowayaio.exceptions import AuthError, CowayError, PasswordExpired
+from cowayaio.exceptions import AuthError, CowayError, PasswordExpired, ServerMaintenance
 
 from homeassistant.core import HomeAssistant
 
@@ -27,11 +27,17 @@ async def async_validate_api(hass: HomeAssistant, username: str, password: str, 
         async with async_timeout.timeout(TIMEOUT):
             await client.login()
             coway_query = await client.async_get_purifiers()
+    except ServerMaintenance as err:
+        raise ServerMaintenance from err
     except AuthError as err:
         LOGGER.error(f'Could not authenticate on Coway servers: {err}')
         raise AuthError from err
     except PasswordExpired as err:
-        LOGGER.error("Coway servers are requesting a password change as the password on this account hasn't been changed for 60 days or more. Use the IoCare app to change your password or use the skip password change option.")
+        LOGGER.error(
+            f"Coway servers are requesting a password change as the password on "
+            f"this account hasn't been changed for 60 days or more. Use the IoCare "
+            f"app to change your password or use the skip password change option."
+        )
         raise PasswordExpired from err
     except COWAY_ERRORS as err:
         LOGGER.error(f'Failed to get information from Coway servers: {err}')
